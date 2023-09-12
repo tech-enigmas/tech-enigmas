1072b7c1c4a775ec0c5364ead27a4bb35cf91fd0
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2023-09-11 19:57:41 -0700

    added post and user schema, added things to server.js

---
+++ src/server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+
+mongoose.connect(process.env.MONGODB_URL);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => console.log('Mongoose is connected'));
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
