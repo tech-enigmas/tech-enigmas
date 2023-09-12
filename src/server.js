6ce7dd1ea83d09673d05230467edf91ea7b521d2
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2023-09-11 20:17:24 -0700

    added basic auth in middleware, added signin and signup functionality to server

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@
 require('dotenv').config();
 const express = require('express');
 const { default: mongoose } = require('mongoose');
+const basic = require('./middleware/basic');
+const bcrypt = require('bcrypt');
 
 const app = express();
 app.use(express.json());
@@ -16,4 +18,18 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', () => console.log('Mongoose is connected'));
 
+app.post('/signup', async (req, res) => {
+  try {
+    req.body.password = await bcrypt.hash(req.body.password, 10);
+    const record = await userModel.create(req.body);
+    res.staus(200).json(record);
+  } catch (e) { res.status(200).send('Error creating user'); }
+});
+
+const handleSignin = async (req, res) => {
+  res.status(200).send(req.user);
+};
+
+app.post('/signin', basic, handleSignin);
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
